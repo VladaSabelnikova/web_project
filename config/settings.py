d31c6a5fdf00c0d43a6b16bca29cd2bb7bcd5de4
vlada <vladasabelnikova@gmail.com>
2022-03-23 01:35:04 +0300

    Разобрались с режимом отладки
    Перенесли конфиг с константой DATABASES
    в отдельный файл config\components\database.py
    В settings.py добавили include для привязки DATABASES с основным файлом настроек.
    
    Так же в settings.py:
        * Вынесли константу DEBUG в переменные окружения.
        * Добавили константу ALLOWED_HOSTS

--- config/settings.py
+++ config/settings.py
@@ -14,8 +14,15 @@ from pathlib import Path
 import os
 
 from dotenv import load_dotenv
+from split_settings.tools import include
+
 
 load_dotenv()
+
+include(
+    'components/database.py',
+)
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -27,9 +34,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', False) == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [os.getenv('DB_HOST', '127.0.0.1')]
 
 
 # Application definition
@@ -72,25 +79,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'config.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST', '127.0.0.1'),
-        'PORT': os.getenv('DB_PORT', 5432),
-        'OPTIONS': {
-            # Нужно явно указать схемы, с которыми будет работать приложение.
-           'options': '-c search_path=public,content'
-        }
-    }
-}
 
 
 # Password validation
