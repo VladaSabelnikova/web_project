1a9ce0d5b073bdbe183c4d9d087348cd8e7906e7
vlada <vladasabelnikova@gmail.com>
2022-03-24 16:40:22 +0300

    Создана первая ORM таблица Langs
    В модели добавлен общий класс IdTimeStampedMixin,
    в котором есть общие для всех таблиц поля id и created_at.
    
    А так же создана первая таблица Langs.
    
    Модель добавлена в admin.py.

--- videos/admin.py
+++ videos/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Langs
+
+
+@admin.register(Langs)
+class LangsAdmin(admin.ModelAdmin):
+    pass
--- videos/models.py
+++ videos/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+import uuid
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class IdTimeStampedMixin(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        abstract = True
+
+
+class Langs(IdTimeStampedMixin):
+    full_title = models.CharField(_('full_title'), max_length=255)
+    iso_639_1 = models.CharField(_('ISO 639-1'), max_length=255)
+    iso_639_2 = models.CharField(_('ISO 639-2'), max_length=255, blank=True)
+
+    class Meta:
+        db_table = 'content"."langs'
+        verbose_name = _('lang')
+        verbose_name_plural = _('langs')
+
+    def __str__(self):
+        return self.full_title
