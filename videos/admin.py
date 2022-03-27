c36865dc8f0c92de89b0e6285ebf1918e579733a
vlada <vladasabelnikova@gmail.com>
2022-03-27 16:07:38 +0300

    Сделали admin.py красивенькой
    Создали:
        * Поля для отображения (list_display)
        * Поля для поиска (search_fields)
        * Поля для фильтрации
        * AudioAndTextInline для отображения аудио и текста внутри видео

--- videos/admin.py
+++ videos/admin.py
@@ -4,16 +4,25 @@ from django.contrib import admin
 from .models import Langs, Video, AudioAndText
 
 
+class AudioAndTextInline(admin.TabularInline):
+    model = AudioAndText
+
+
 @admin.register(Langs)
 class LangsAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('full_title', 'iso_639_1', 'created_at',)
+    search_fields = ('id', 'full_title', 'iso_639_1', 'iso_639_2')
 
 
 @admin.register(Video)
 class VideoAdmin(admin.ModelAdmin):
-    pass
+    inlines = (AudioAndTextInline,)
+    list_display = ('path_to_video', 'updated_at')
+    search_fields = ('id',)
 
 
 @admin.register(AudioAndText)
 class AudioAndTextAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('title', 'path_to_audio', 'updated_at')
+    list_filter = ('video',)
+    search_fields = ('title', 'h1', 'id', 'description')
