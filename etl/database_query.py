e0c73eb350bd0d587e3795917bc1b85fb2404ce2
vlada <vladasabelnikova@gmail.com>
2022-04-11 20:26:24 +0300

    Подправили запрос к базе данных database_query.py (кавычки)
    Фигурные скобки, где по задумке будет вставлена дата,
    обернуты в ординарные кавычки, т.к. дата будет строкой.
    
    Было — {}
    Стало '{}'

--- etl/database_query.py
+++ etl/database_query.py
@@ -11,16 +11,16 @@ SELECT_FROM_PG = """
         v.h1,
         at.audio_file,
         vt.video_file,
-        v.updated_at > {} as data_changed,
-        at.updated_at > {} OR vt.updated_at > {} as track_changed,
+        v.updated_at > '{}' as data_changed,
+        at.updated_at > '{}' OR vt.updated_at > '{}' as track_changed,
         GREATEST(v.updated_at, at.updated_at, vt.updated_at) as max_date
 
     FROM content.video v
     LEFT JOIN content.audio_track at on at.id = v.audio_track_id
     LEFT JOIN content.video_track vt on vt.id = v.video_track_id
     WHERE
-        v.updated_at > {} OR
-        at.updated_at > {} OR
-        vt.updated_at > {}
+        v.updated_at > '{}' OR
+        at.updated_at > '{}' OR
+        vt.updated_at > '{}'
     LIMIT {} OFFSET {}
 """
