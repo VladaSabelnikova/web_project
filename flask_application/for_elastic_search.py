68d7dc40ab1e905021973b70be18481a4f9a465a
vlada <vladasabelnikova@gmail.com>
2022-04-15 18:01:52 +0300

    Класс, для работы с elastic search готов (версия 1)
    Создан класс с двумя методами:
        * get_all (достанет весь контент из эластика)
        * get_by_id (достанет документ по его id)

---
+++ flask_application/for_elastic_search.py
@@ -0,0 +1,14 @@
+from elasticsearch import Elasticsearch
+
+
+class ElasticSearchMethods:
+
+    def __init__(self, address, index_name):
+        self.address, self.index_name = address, index_name
+        self.es = Elasticsearch(self.address)
+
+    def get_all(self):
+        return self.es.search(index=self.index_name, query={'match_all': {}})
+
+    def get_by_id(self, id: str):
+        return self.es.get(index=self.index_name, id=id)
