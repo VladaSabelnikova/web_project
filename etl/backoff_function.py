209b98868cb8791cb0319e2f44269839461cea55
vlada <vladasabelnikova@gmail.com>
2022-04-17 19:50:50 +0300

    Переименовали строки в логгах на латиницу в backoff_function.py
    Теперь все логги на английском, так лучше для докера.
    Иначе в консоли докера пишет какую-то хрень не читаемую.
    
    Кроме того название файла теперь передается через __file__,
    а не через __name__
    
    logger = create_logger(__file__, stream_out=sys.stderr)
    
    Теперь понятно, откуда лог вообще взялся.

--- etl/backoff_function.py
+++ etl/backoff_function.py
@@ -66,7 +66,7 @@ def backoff(
                     return func(*args, **kwargs)
                 except Exception as error:
 
-                    logger.error('%s\nПовтор через %s секунд.\n', error, start_sleep_time)
+                    logger.error('%s\nWill repeat in %s seconds.\n', error, start_sleep_time)
 
                     sleep(start_sleep_time)
                     start_sleep_time = min(start_sleep_time * factor, border_sleep_time)  # noqa: WPS442
@@ -76,4 +76,4 @@ def backoff(
     return func_wrapper
 
 
-logger = create_logger(__name__, stream_out=sys.stderr)
+logger = create_logger(__file__, stream_out=sys.stderr)
