efc8fef49c2793eedd9e73aa0cb805cbd504ed78
vlada <vladasabelnikova@gmail.com>
2022-04-20 19:05:42 +0300

    Добавили в настройки django admin ALLOWED_HOSTS = ['current host']
    Трахаться придётся всё таки.

--- django_admin/config/settings/base.py
+++ django_admin/config/settings/base.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = os.getenv('DEBUG', False) == 'True'
 
 # ALLOWED_HOSTS = [os.getenv('DB_HOST', '127.0.0.1')]
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['51.250.8.64']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
