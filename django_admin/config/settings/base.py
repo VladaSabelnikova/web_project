4b7b13347e61e8858a7df277aaa87df5701d01e3
vlada <vladasabelnikova@gmail.com>
2022-04-20 21:09:14 +0300

    ALLOWED_HOSTS снова звёздочка
    Думаем, что проблема с облаком, а не со звёздочкой.
    Поэтому пробуем еще раз её применить.

--- django_admin/config/settings/base.py
+++ django_admin/config/settings/base.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = os.getenv('DEBUG', False) == 'True'
 
 # ALLOWED_HOSTS = [os.getenv('DB_HOST', '127.0.0.1')]
-ALLOWED_HOSTS = ['51.250.8.64']
+ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
